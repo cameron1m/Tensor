29863cfac1dff3fc9f9a8289e380e16a20f3a01c
cameron1m <45009513+cameron1m@users.noreply.github.com>
2020-07-17 17:11:54 -0700

    removed local library

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import
-function test(){
-    
+function setup(){
+    const values = [];
+    for(let i = 0; i < 15; i++){
+        values[i] = Math.random()*100;
+    }
+    const shape = [5,3];
+
+
+
+    const data = tf.tensor(values, shape)
+
+    console.log(data.toString());
+    // console.log(data);
 }
-test();
+setup();
